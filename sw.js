5acb5ada6b632b808eea74935fd05a0714ce09f4
Ratman407 <rixon18@hotmail.com>
2026-09-11 19:39:26 +1000

    Refresh service worker after loading freeze fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='bushtrack-v064-fix3-cache';
-const KEEP=['bushtrack-v064-fix3-cache','bushtrack-v064-fix2-cache','bushtrack-v064-fix1-cache','bushtrack-v064-cache','bushtrack-v061-cache','bushtrack-v06-cache','bushtrack-v05-cache','bushtrack-v04-fallback-cache','bushtrack-v04-cache'];
+const CACHE='bushtrack-v064-fix4-cache';
+const KEEP=['bushtrack-v064-fix4-cache','bushtrack-v064-fix3-cache','bushtrack-v064-fix2-cache','bushtrack-v064-fix1-cache','bushtrack-v064-cache','bushtrack-v061-cache','bushtrack-v06-cache','bushtrack-v05-cache','bushtrack-v04-fallback-cache','bushtrack-v04-cache'];
 const FILES=['./','./index.html','./style-v06.css','./style-v061.css','./app-v06.js','./hotfix-v061.js','./stats-v064.js','./updatefix-v064.js','./manifest.webmanifest','./version.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./fallback-v05.html','./style-v05.css','./app-v05.js','./fallback-v04.html','./style-v04.css','./app-v04.js'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>!KEEP.includes(k)).map(k=>caches.delete(k)))),self.clients.claim()]))});
